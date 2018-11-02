f3beec1821b41289fe6e04466f296796e4769c4f
jqkiger <jqkiger7@gmail.com>
2018-11-01 23:58:13 -0700

    Updates

---
+++ precache-manifest.4a84b8f5011eaab7ee074eb1ef7443db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ede091ae0b6eae1c6624",
+    "url": "/COGS120-JAY/static/js/runtime~main.ede091ae.js"
+  },
+  {
+    "revision": "9cd08ab0b2daa371bc99",
+    "url": "/COGS120-JAY/static/js/main.9cd08ab0.chunk.js"
+  },
+  {
+    "revision": "bda349d87f25c593e5a8",
+    "url": "/COGS120-JAY/static/js/1.bda349d8.chunk.js"
+  },
+  {
+    "revision": "e35e4ab48afbe3eea0927e4b089cbfe3",
+    "url": "/COGS120-JAY/index.html"
+  }
+];
