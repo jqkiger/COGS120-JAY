31165f1cbee24abc91587ce5eabcfd092f655a0d
jqkiger <jqkiger7@gmail.com>
2018-11-01 23:59:50 -0700

    GH pages SUKS!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
