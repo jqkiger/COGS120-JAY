495677d9a0c1cd72591801935bc5122d333364ae
jqkiger <jqkiger7@gmail.com>
2018-11-01 23:57:36 -0700

    GH pages SUKS!

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
 class friends extends React.Component {
